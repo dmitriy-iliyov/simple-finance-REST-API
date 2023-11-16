b26589b21f7d66d2b7a10a623b2c7c1a64aa8232
Dima Iliov <dmitriy.iliyov@gmail.com>
2023-11-16 12:45:43 +0200

    add marshmellows

--- lab_3/code/marshmallows.py
+++ lab_3/code/marshmallows.py
@@ -1,3 +1,19 @@
 from marshmallow import Schema, fields
 
 
+class UserSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+
+
+class CategorySchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+
+
+class RecodSchema(Schema):
+    id = fields.Str(dump_only=True)
+    userID = fields.Str(required=True)
+    categoryID = fields.Str(required=True)
+    time = fields.Str(dump_only=True)
+    amountOfExpenditure = fields.Str(required=True)
