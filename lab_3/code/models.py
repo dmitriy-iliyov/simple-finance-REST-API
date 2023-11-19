ae364ff5fb4df2b5eebdce66d72665fe871860df
Dima Iliov <dmitriy.iliyov@gmail.com>
2023-11-19 06:21:38 +0200

    add account model

--- lab_3/code/models.py
+++ lab_3/code/models.py
@@ -8,10 +8,25 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), unique=True, nullable=False)
 
+    user_money = db.relationship("BankAccountModel", uselist=False, back_populates="user_account")
     record = db.relationship("RecordModel", back_populates="user", lazy="dynamic")
 
     def __init__(self, name):
         self.name = name
+
+
+class BankAccountModel(db.Model):
+    __tablename__ = "accounts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), unique=True)
+    money = db.Column(db.Float(precision=2), unique=False, nullable=False)
+
+    user_account = db.relationship("UserModel", back_populates="user_money")
+
+    def __init__(self, user_id, money):
+        self.user_id = user_id
+        self.money = money
 
 
 class CategoryModel(db.Model):
